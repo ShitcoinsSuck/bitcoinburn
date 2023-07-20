c3053a9665864a8b208622f53d34ccb52a34c466
ShitcoinsSuck <ShitcoinsSuck@github.com>
2023-07-20 13:17:14 -0500

    initially adding getburns.py to output burns for a given block range in csv format

---
+++ contrib/burnextraction/getburns.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+import os
+import json
+import sys
+import getopt
+import datetime
+import csv
+
+includeZeroValue="false"
+
+#datadir='/mnt/bitcoin/bitcoin'
+#rawexe='~/bitcoin/src/bitcoin-cli'
+#exe=f"{rawexe} -datadir={datadir}"
+
+
+writer = csv.writer(open('burns.csv', 'w'), delimiter=',', lineterminator='\n', quoting=csv.QUOTE_MINIMAL, escapechar='\\')
+row=["blockheight", "time", "value", "txid", "destination", "type", "message"]
+writer.writerow(row);
+
+
+NOPRINT_TRANS_TABLE = {
+    i: None for i in range(0, sys.maxunicode + 1) if not chr(i).isprintable()
+}
+def make_printable(s):
+    """Replace non-printable characters in a string."""
+
+    # the translate method on str removes characters
+    # that map to None from the string
+    return s.translate(NOPRINT_TRANS_TABLE)
+
+def get_block_opreturns(blocknumber):
+    command=f"{exe} getburnsblock \"{blocknumber}\" {includeZeroValue}"
+    if(0==blocknumber % 100):
+        print(command)
+    stream = os.popen(command)
+    output = stream.read()
+    #print("OUTPUT:", output)
+    parsed=json.loads(output)
+    if(parsed.get('time') is None):
+        return
+    dt = datetime.datetime.fromtimestamp(parsed['time'])
+    blocktime = dt.strftime("%Y%m%d %H:%M:%S")
+    blockheight=blocknumber
+    blockhash=parsed['hash']
+    for opreturn in parsed['burns']:
+        value=opreturn['value']
+        txid = opreturn['txid'];
+        destination = opreturn['destination']
+        destinationType = opreturn['type']
+        rawmessage = opreturn['message']
+        message = make_printable(bytes.fromhex(rawmessage).decode('utf-8', errors='ignore'))
+        row=[blockheight, blocktime, value, txid, destination, destinationType, message]
+        writer.writerow(row)
+        #print(row)
+    return
+
+def usage(retval):
+    print('getburns.py -s <startblock> -e <endblock> -d <datadir> -c <clidir> [-z]\n')
+    print('\tnote datadir refers to the datadir of bitcoin core and clidir refers to the dir where bitcoin-cli is located')
+    sys.exit(retval)
+    return
+
+def main(argv):
+    global includeZeroValue
+    global datadir
+    global clidir
+    global exe
+    try:
+        opts, args = getopt.getopt(argv,"hzs:e:d:c:",["startblock=", "endblock=", "datadir=", "clidir="])
+    except:
+        print('getburns.py -s <startblock> -e <endblock> -d <datadir> -c <clidir> [-z]')
+        usage(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            usage(0)
+        elif opt in ("-s", "--startblock"):
+            startblock=int(arg)
+        elif opt in ("-e", "--endblock"):
+            endblock=int(arg)
+        elif opt in ("-d", "--datadir"):
+            datadir=arg
+        elif opt in ("-c", "--clidir"):
+            clidir=arg
+        elif opt in ("-z"):
+            includeZeroValue="true"
+    try: 
+        startblock
+        endblock
+        datadir
+        clidir
+    except NameError: usage(3)
+
+    rawexe=F"{clidir}/bitcoin-cli"
+    exe=f"{rawexe} -datadir={datadir}"
+
+    print(f"start block is {startblock} end block is {endblock}")
+
+    blockrange=range(startblock, endblock+1)
+    for height in reversed(blockrange):
+        get_block_opreturns(height)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
